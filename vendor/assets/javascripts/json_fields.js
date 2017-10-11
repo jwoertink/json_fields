cac13017925e0e50ac91f78eabd186024b3099e4
Jeremy Woertink <jeremywoertink@gmail.com>
2017-10-11 16:37:39 -0700

    the javascript that will be needed

---
+++ vendor/assets/javascripts/json_fields.js
@@ -0,0 +1,23 @@
+$(function() {
+
+  // Add additional fields to the form
+  $(document).on('click', '.add-json-fields', function() {
+    $target = $("#"+$(this).data('target'));
+    template = $target.find('.template:last').html();
+    if(typeof template === "undefined" || template === "") {
+      return false;
+    }
+    r = new RegExp('#' + $(this).data('target') + "\\[(\\d+)\\]", "g");
+    current_num = parseInt(r.exec(template)[1]);
+    new_template = template.replace(r, $(this).data('target')+'['+(current_num+1)+']');
+    $new_row = $('<div class="row template">'+new_template+'</div>');
+    $target.append($new_row);
+    return false;
+  });
+
+  // Remove the fields from the form
+  $(document).on('click', '.remove-json-fields', function() {
+    $(this).parents('.row.template:first').remove();
+    return false;
+  });
+});
